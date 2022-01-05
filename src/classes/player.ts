04df8da86ebebc2802fc08637864ea3074f879bb
Rafał Rudko <rafal.rudko@rb.com>
2022-01-05 18:52:04 +0100

    remove unnecessary keys and add space WIP action

--- src/classes/player.ts
+++ src/classes/player.ts
@@ -1,27 +1,13 @@
 import { Actor } from "./actor";
 
 export class Player extends Actor {
-  private keyW: Phaser.Input.Keyboard.Key;
-  private keyA: Phaser.Input.Keyboard.Key;
-  private keyS: Phaser.Input.Keyboard.Key;
-  private keyD: Phaser.Input.Keyboard.Key;
-  private keyUp: Phaser.Input.Keyboard.Key;
-  private keyLeft: Phaser.Input.Keyboard.Key;
-  private keyDown: Phaser.Input.Keyboard.Key;
-  private keyRight: Phaser.Input.Keyboard.Key;
+  private keySpace: Phaser.Input.Keyboard.Key;
 
   constructor(scene: Phaser.Scene) {
     super(scene, 0, 0, "astronaut");
 
     // KEYS
-    this.keyW = this.scene.input.keyboard.addKey("W");
-    this.keyA = this.scene.input.keyboard.addKey("A");
-    this.keyS = this.scene.input.keyboard.addKey("S");
-    this.keyD = this.scene.input.keyboard.addKey("D");
-    this.keyUp = this.scene.input.keyboard.addKey("UP");
-    this.keyLeft = this.scene.input.keyboard.addKey("LEFT");
-    this.keyDown = this.scene.input.keyboard.addKey("DOWN");
-    this.keyRight = this.scene.input.keyboard.addKey("RIGHT");
+    this.keySpace = this.scene.input.keyboard.addKey("Space");
 
     // PHYSICS
     this.setCircle(8);
@@ -41,6 +27,10 @@ export class Player extends Actor {
       gridEngine.move("player", "up");
     } else if (cursors.down.isDown) {
       gridEngine.move("player", "down");
+    }
+
+    if (this.keySpace?.isDown) {
+      console.log("some future action");
     }
   }
 
