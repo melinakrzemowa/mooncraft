c54af51dc9c49e208411574c98adf33b963a0658
Bartosz Kalinowski <kelostrada@gmail.com>
2022-01-15 15:48:55 +0100

    fix prettier styles

--- src/scenes/loading/index.ts
+++ src/scenes/loading/index.ts
@@ -5,16 +5,17 @@ export class LoadingScene extends Scene {
   private singlePlayerText!: Phaser.GameObjects.Text;
   private multiPlayerText!: Phaser.GameObjects.Text;
 
-
   constructor() {
     super("loading-scene");
   }
 
   init() {
-    this.loadingText = this.add.text(2080 / 2, 1440 / 2,
-      'Loading ...', { font: '64pt ThaleahFat', color: '#FFFFFF', align: 'center' }
-    )
-    this.loadingText.setOrigin(0.5, 0.5)
+    this.loadingText = this.add.text(2080 / 2, 1440 / 2, "Loading ...", {
+      font: "64pt ThaleahFat",
+      color: "#FFFFFF",
+      align: "center",
+    });
+    this.loadingText.setOrigin(0.5, 0.5);
   }
 
   preload(): void {
@@ -53,44 +54,44 @@ export class LoadingScene extends Scene {
 
   create(): void {
     // Remove loading text
-    this.loadingText.destroy()
+    this.loadingText.destroy();
 
-    this.singlePlayerText = this.createButton(2080 / 2, 1440 / 2 - 100, 'SINGLE PLAYER')
-    this.multiPlayerText = this.createButton(2080 / 2, 1440 / 2 + 100, 'MULTI PLAYER')
+    this.singlePlayerText = this.createButton(2080 / 2, 1440 / 2 - 100, "SINGLE PLAYER");
+    this.multiPlayerText = this.createButton(2080 / 2, 1440 / 2 + 100, "MULTI PLAYER");
 
-    this.singlePlayerText.on('pointerdown', () => this.startSinglePlayer())
-    this.multiPlayerText.on('pointerdown', () => this.startMultiPlayer())
+    this.singlePlayerText.on("pointerdown", () => this.startSinglePlayer());
+    this.multiPlayerText.on("pointerdown", () => this.startMultiPlayer());
   }
 
   startSinglePlayer(): void {
-    this.singlePlayerText.destroy()
-    this.multiPlayerText.destroy()
+    this.singlePlayerText.destroy();
+    this.multiPlayerText.destroy();
 
     this.registry.set("playerPosition", { x: 25, y: 26 });
     this.scene.start("moon-scene");
   }
 
   startMultiPlayer(): void {
-    this.singlePlayerText.destroy()
-    this.multiPlayerText.destroy()
+    this.singlePlayerText.destroy();
+    this.multiPlayerText.destroy();
 
     this.scene.start("multi-scene");
   }
 
   createButton(x: number, y: number, text: string): Phaser.GameObjects.Text {
-    const button = this.add.text(x, y, text, { font: '128pt ThaleahFat', color: '#FFFFFF', align: 'center' })
-    button.setOrigin(0.5, 0.5)
+    const button = this.add.text(x, y, text, { font: "128pt ThaleahFat", color: "#FFFFFF", align: "center" });
+    button.setOrigin(0.5, 0.5);
     button.setShadow(10, 5, "#333333", 0, false, true);
-    button.setInteractive()
+    button.setInteractive();
 
-    button.on('pointerover', () => {
-      button.setColor("#6b6688")
-    })
+    button.on("pointerover", () => {
+      button.setColor("#6b6688");
+    });
 
-    button.on('pointerout', () => {
-      button.setColor("#FFFFFF")
-    })
+    button.on("pointerout", () => {
+      button.setColor("#FFFFFF");
+    });
 
-    return button
+    return button;
   }
 }
